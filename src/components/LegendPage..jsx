9981befe4c4a82d9eae8a23a5fc30d22a4940a48
Sam Woody <swoodall2527@gmail.com>
2025-07-29 10:48:08 +0100

    styling changes

--- src/components/LegendPage..jsx
+++ src/components/LegendPage..jsx
@@ -13,90 +13,105 @@ function LegendPage() {
         <div>
             <BackHeader />
 
+            <div className='options-container'>
+                <div className='tile-container'>
+                    <div className='tile-container-header'>
+                        <h2>Scoring Tile Information</h2>
+                        <button onClick={() => setScoringHide(!scoringHide)}>
+                            {scoringHide ? 'Show' : 'Hide'}
+                        </button>
+                    </div>
 
-            <div className='tile-container'>
-                <div className='tile-container-header'>
-                    <h2>Scoring Tile Information</h2>
-                    <button onClick={() => setScoringHide(!scoringHide)}>
-                        {scoringHide ? 'Show' : 'Hide'}
-                    </button>
+                    <div className='tile-widget-container'>
+                    {!scoringHide &&
+                        Object.keys(tileInfo.scoreTiles).map((key) => (
+                            <div className='tile-widget' key={key}>
+                                <div className='tile-widget-main'>
+                                    <h3>{key}</h3>
+                                    <img className='tile-widget-img-normal' src={tileInfo.scoreTiles[key].img} alt={key} />
+                                </div>
+                                <p>{tileInfo.scoreTiles[key].info}</p>
+                            </div>
+                        ))}
+                    </div>
+
                 </div>
 
-                {!scoringHide &&
-                    Object.keys(tileInfo.scoreTiles).map((key) => (
-                        <div className='tile-widget' key={key}>
-                            <div className='tile-widget-main'>
-                                <h3>{key}</h3>
-                                <img className='tile-widget-img-normal' src={tileInfo.scoreTiles[key].img} alt={key} />
+
+                <div className='tile-container'>
+                    <div className='tile-container-header'>
+                        <h2>Bonus Tile Information</h2>
+                        <button onClick={() => setBonusHide(!bonusHide)}>
+                            {bonusHide ? 'Show' : 'Hide'}
+                        </button>
+                    </div>
+
+
+                    <div className='tile-widget-container'>
+                    {!bonusHide &&
+                        Object.keys(tileInfo.bonusTiles).map((key) => (
+                            <div className='tile-widget' key={key}>
+                                <div className='tile-widget-main'>
+                                    <h3>{key}</h3>
+                                    <img className='bonus-tile-img' src={tileInfo.bonusTiles[key].img} alt={key} />
+                                </div>
+                                <p>{tileInfo.bonusTiles[key].info}</p>
                             </div>
-                            <p>{tileInfo.scoreTiles[key].info}</p>
-                        </div>
-                    ))}
-            </div>
+                        ))}
 
-
-            <div className='tile-container'>
-                <div className='tile-container-header'>
-                    <h2>Bonus Tile Information</h2>
-                    <button onClick={() => setBonusHide(!bonusHide)}>
-                        {bonusHide ? 'Show' : 'Hide'}
-                    </button>
+                    </div>
                 </div>
 
-                {!bonusHide &&
-                    Object.keys(tileInfo.bonusTiles).map((key) => (
-                        <div className='tile-widget' key={key}>
-                            <div className='tile-widget-main'>
-                                <h3>{key}</h3>
-                                <img className='bonus-tile-img' src={tileInfo.bonusTiles[key].img} alt={key} />
+
+                <div className='tile-container'>
+                    <div className='tile-container-header'>
+                        <h2>Favor Tile Information</h2>
+                        <button onClick={() => setFavorHide(!favorHide)}>
+                            {favorHide ? 'Show' : 'Hide'}
+                        </button>
+                    </div>
+
+
+                    <div className='tile-widget-container'>
+                    {!favorHide &&
+                        Object.keys(tileInfo.favorTiles).map((key) => (
+                            <div className='tile-widget' key={key}>
+                                <div className='tile-widget-main'>
+                                    <h3>{key}</h3>
+                                    <img className='tile-widget-img-normal' src={tileInfo.favorTiles[key].img} alt={key} />
+                                </div>
+                                <p>{tileInfo.favorTiles[key].info}</p>
                             </div>
-                            <p>{tileInfo.bonusTiles[key].info}</p>
+                        ))}
+
                         </div>
-                    ))}
-            </div>
-
-
-            <div className='tile-container'>
-                <div className='tile-container-header'>
-                    <h2>Favor Tile Information</h2>
-                    <button onClick={() => setFavorHide(!favorHide)}>
-                        {favorHide ? 'Show' : 'Hide'}
-                    </button>
                 </div>
 
-                {!favorHide &&
-                    Object.keys(tileInfo.favorTiles).map((key) => (
-                        <div className='tile-widget' key={key}>
-                            <div className='tile-widget-main'>
-                                <h3>{key}</h3>
-                                <img className='tile-widget-img-normal' src={tileInfo.favorTiles[key].img} alt={key} />
+
+                <div className='tile-container'>
+                    <div className='tile-container-header'>
+                        <h2>Town Tile Information</h2>
+                        <button onClick={() => setTownHide(!townHide)}>
+                            {townHide ? 'Show' : 'Hide'}
+                        </button>
+                    </div>
+
+
+                        <div className='tile-widget-container'>
+                    {!townHide &&
+                        Object.keys(tileInfo.townTiles).map((key) => (
+                            <div className='tile-widget' key={key}>
+                                <div className='tile-widget-main'>
+                                    <h3>{key}</h3>
+                                    <img className='tile-widget-img-normal' src={tileInfo.townTiles[key].img} alt={key} />
+                                </div>
+                                <p>{tileInfo.townTiles[key].info}</p>
                             </div>
-                            <p>{tileInfo.favorTiles[key].info}</p>
+                        ))}
+
                         </div>
-                    ))}
-            </div>
-
-
-            <div className='tile-container'>
-                <div className='tile-container-header'>
-                    <h2>Town Tile Information</h2>
-                    <button onClick={() => setTownHide(!townHide)}>
-                        {townHide ? 'Show' : 'Hide'}
-                    </button>
                 </div>
-
-                {!townHide &&
-                    Object.keys(tileInfo.townTiles).map((key) => (
-                        <div className='tile-widget' key={key}>
-                            <div className='tile-widget-main'>
-                                <h3>{key}</h3>
-                                <img className='tile-widget-img-normal' src={tileInfo.townTiles[key].img} alt={key} />
-                            </div>
-                            <p>{tileInfo.townTiles[key].info}</p>
-                        </div>
-                    ))}
             </div>
-
 
 
         </div >
