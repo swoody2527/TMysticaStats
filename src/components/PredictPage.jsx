4245fb4ca114e87382f281c4358bcbb96b12fb60
Sam Woody <swoodall2527@gmail.com>
2025-08-02 00:15:07 +0100

    front end predictions

--- src/components/PredictPage.jsx
+++ src/components/PredictPage.jsx
@@ -1,13 +1,117 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { Link, useSearchParams } from 'react-router-dom'
 import BackHeader from './reusable/BackHeader'
 import PredictFilter from './reusable/PredictFilter'
+import '../styles/PredictPage.css'
+import axios from 'axios'
+import { factionInformation } from '../assets/infoDicts'
+import factionImages from '../assets/faction-images/factionImages'
 
 function PredictPage() {
+  const [searchParams, setSearchParams] = useSearchParams()
+  const [isLoading, setIsLoading] = useState(true)
+  const [hasSearched, setHasSearched] = useState(false)
+  const [error, setError] = useState(false)
+  const [isDesktop, setIsDesktop] = useState(window.innerWidth > 1024)
+
+
+  const isFactions = searchParams.get('isFactions') === 'true';
+  const numPlayers = searchParams.get('numPlayers');
+  const mapID = searchParams.get('mapID');
+  const scoreTileList = searchParams.getAll('scoreTile');
+  const bonusTileList = searchParams.getAll('bonusTile');
+  const factionList = searchParams.getAll('faction');
+
+  const [predictionData, setPredictionData] = useState({});
+
+  const handleFilterSubmit = (data) => {
+    setHasSearched(true);
+
+    const params = new URLSearchParams();
+
+    if (data.isFactions) params.set('isFactions', String(data.isFactions))
+
+    if (data.numPlayers) params.set('numPlayers', data.numPlayers);
+    if (data.mapID) params.set('mapID', data.mapID);
+
+    data.scoreTileList.forEach(tile => params.append('scoreTile', tile));
+    data.bonusTileList.forEach(tile => params.append('bonusTile', tile));
+    data.factionList.forEach(faction => params.append('faction', faction));
+
+    setSearchParams(params);
+  };
+
+
+  useEffect(() => {
+    setError(false)
+    setIsLoading(true)
+    const shouldFetch = numPlayers && mapID && bonusTileList && scoreTileList && factionList
+    if (!shouldFetch) return;
+
+    const axiosParams = new URLSearchParams();
+
+    axiosParams.set('num_players', Number(numPlayers));
+    axiosParams.set('map_id', mapID.toUpperCase());
+    axiosParams.set('factions_included', String(isFactions));
+
+    scoreTileList.forEach(tile => axiosParams.append('score_tiles', tile));
+    bonusTileList.forEach(tile => axiosParams.append('bonus_tiles', tile));
+    factionList.forEach(faction => axiosParams.append('game_factions', faction));
+
+
+    const fetchData = async () => {
+      try {
+        const predictionData = await axios.get('https://tmysticastats-api-production.up.railway.app/api/predictions/win_prediction', {
+          params: axiosParams
+        })
+
+        console.log(predictionData)
+
+        const sortedByWinProb = Object.entries(predictionData.data)
+          .sort(([, a], [, b]) => b.win_prob - a.win_prob)
+          .reduce((obj, [key, value]) => {
+            obj[key] = value;
+            return obj;
+          }, {});
+
+
+
+
+        setPredictionData(sortedByWinProb)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+    fetchData()
+  }, [searchParams.toString()])
+
+
   return (
-    <div>
-        <BackHeader />
-        <PredictFilter/>
+    <div className='prediction-page'>
+      <BackHeader />
+      <PredictFilter onSubmit={handleFilterSubmit} />
+      <div className='prediction-header'>
+        <h2>Prediction Results</h2>
+      </div>
+      <div className='prediction-output-container'>
+        <div className='faction-prediction header-row'>
+          <p className='header-cell'>Faction</p>
+          <p className='header-cell'>Win Chance</p>
+          <p className='header-cell'>Risk Level</p>
+        </div>
+        {Object.keys(predictionData).map(faction => (
+          <div className='faction-prediction'>
+            <p style={{
+              backgroundImage: `url(${factionImages[factionInformation[faction.toLowerCase()].name.split(' ').join('')]})`,
+              backgroundSize: 'cover',    
+              backgroundPosition: 'center',                            
+            }}>{factionInformation[faction.toLowerCase()].name}</p>
+            <p>{(predictionData[faction].win_prob * 100).toFixed(2)}%</p>
+            <p>{predictionData[faction].risk_level}</p>
+          </div>
+        ))}
+
+      </div>
     </div>
   )
 }
--- src/components/reusable/PredictFilter.jsx
+++ src/components/reusable/PredictFilter.jsx
@@ -4,24 +4,102 @@ import { mapInformation, factionInformation } from '../../assets/infoDicts';
 import tileInfo from '../../assets/tileInfo';
 import factionImages from '../../assets/faction-images/factionImages';
 
-function PredictFilter() {
-    const [isExpanded, setIsExpanded] = useState(false);
+function PredictFilter({ onSubmit, initialValues = {} }) {
+    const [isExpanded, setIsExpanded] = useState(true);
+    const [numPlayers, setNumPlayers] = useState(null)
+    const [map, setMap] = useState(null)
+    const [scoreTileList, setScoreTileList] = useState([])
+    const [bonusTileList, setBonusTileList] = useState([])
+    const [factionList, setFactionList] = useState([])
+    const [factionsIncluded, setFactionsIncluded] = useState(true)
+
 
     const handleFilterToggle = () => {
         setIsExpanded(!isExpanded);
     };
 
 
-    const bonusTiles = Object.keys(tileInfo['bonusTiles'])
-    const num_players = [2, 3, 4, 5,]
-    const scoreTiles = Object.keys(tileInfo['scoreTiles'])
-    const factions = Object.keys(factionInformation)
+    const handleScoreTileChange = (e) => {
+        const { value, checked } = e.target
+
+        setScoreTileList(prev => {
+            if (checked) {
+                if (prev.length == 6) {
+                    return prev
+                } else {
+                    return [...prev, value]
+                }
+            } else {
+                return prev.filter(tile => tile !== value)
+            }
+        })
+
+    }
+
     const bonusTileAmounts = {
         2: 5,
         3: 6,
         4: 7,
         5: 8,
     }
+    const handleBonusTileChange = (e) => {
+        const { value, checked } = e.target
+
+        setBonusTileList(prev => {
+            if (checked) {
+                if (prev.length == bonusTileAmounts[numPlayers]) {
+                    return prev
+                } else {
+                    return [...prev, value]
+                }
+            } else {
+                return prev.filter(tile => tile !== value)
+            }
+        })
+
+    }
+
+    {/*
+    const handleFactionListChange = (e) => {
+        const { value, checked } = e.target
+
+        setFactionList(prev => {
+            if (checked) {
+                if (prev.length == numPlayers - 1) {
+                    return prev
+                } else {
+                    return [...prev, value]
+                }
+            } else {
+                return prev.filter(fact => fact !== value)
+            }
+        })
+
+    }
+        */}
+
+
+    const handleFilterSubmit = (e) => {
+        e.preventDefault()
+
+        let data = {
+            isFactions: false,
+            numPlayers: numPlayers,
+            mapID: map,
+            scoreTileList: scoreTileList,
+            bonusTileList: bonusTileList,
+            factionList: factionList
+        }
+        setIsExpanded(false)
+        onSubmit(data)
+    }
+
+
+    const bonusTiles = Object.keys(tileInfo['bonusTiles'])
+    const num_players = [2, 3, 4, 5,]
+    const scoreTiles = Object.keys(tileInfo['scoreTiles'])
+    const factions = Object.keys(factionInformation)
+
 
     return (
         <div className={`filter-container ${isExpanded ? 'filter-container-expanded' : 'filter-container-banner'}`}>
@@ -31,12 +109,63 @@ function PredictFilter() {
 
             {isExpanded && (
                 <>
-                    <form className='filter-options-container'>
+                    <form className='filter-options-container' onSubmit={handleFilterSubmit}>
+
+                        <div className='filter-option input-info'>
+                            <h2>Faction Suggestions</h2>
+                            <p style={{padding: 20}}>
+                                The faction suggestion tool can give rough guidance
+                                on how factions will perform given the amount of players,
+                                the map, the scoring tiles, and the bonus tiles. Some combinations
+                                of these variables are extremely rare and it is possible for some
+                                predictions to be off to large degree.
+                            </p>
+                            <p>Scoring tiles should be entered in order of their round occurance.</p>
+
+                        </div>
+
+
+                        <div className='filter-option input-info'>
+                            <h2>Current Inputs</h2>
+                            <div className='info-holder'>
+                                <p className='info-holder-title'>No. Players</p>
+                                <p className='info-holder-data'>{numPlayers || 'Not Selected'}</p>
+                            </div>
+                            <div className='info-holder'>
+                                <p className='info-holder-title'>Map</p>
+                                {map ? <p className='info-holder-data'>{mapInformation[map].map_name}</p > : <p className='info-holder-data'>Not Selected</p>}
+                            </div>
+
+                            <div className='info-holder'>
+                                <p className='info-holder-title'>Score Tiles: {scoreTileList.length}/6</p>
+                                <p className='info-holder-data'>{scoreTileList.join(', ') || 'None Selected'}</p>
+                            </div>
+                            
+                            <div className='info-holder'>
+                                <p className='info-holder-title'>Bonus Tiles: {bonusTileList.length}/{bonusTileAmounts[numPlayers] || '?'}</p>
+                                <p className='info-holder-data'>{bonusTileList.join(', ') || 'None Selected'}</p>
+                            </div>
+
+                            {/*
+                            {numPlayers ? <p>Factions: {factionList.length}/{numPlayers-1}</p> : <p>Factions</p>}
+                            <p>Factions Unknown?</p>
+                            <input type='checkbox' checked={!factionsIncluded} onChange={() => {setFactionsIncluded(!factionsIncluded)}}></input>                     
+                            <p>{factionList}</p>
+                            */}
+                            <button className='apply-btn' type='submit'>Predict</button>
+                        </div>
+
+                        
+
 
                         <div className='filter-option'>
                             <p>No. Players</p>
                             <div className='filter-selections'>
-                                <select className='option-select'>
+                                <select onChange={(e) => {
+                                    setNumPlayers(e.target.value)
+                                    setBonusTileList([])
+                                }} className='option-select'>
+                                    <option selected value="" disabled>Select Players</option>
                                     {num_players.map((num, index) => (
                                         <option key={num}>{num}</option>
                                     ))}
@@ -48,9 +177,10 @@ function PredictFilter() {
                         <div className='filter-option'>
                             <p>Map</p>
                             <div className='filter-selections'>
-                                <select className='option-select map-filter'>
+                                <select onChange={(e) => setMap(e.target.value)} className='option-select map-filter'>
+                                    <option selected value="" disabled>Select Map</option>
                                     {Object.keys(mapInformation).map((map_id, index) => (
-                                        <option value={map_id.toUpperCase()} key={map_id}>{mapInformation[map_id].map_name}</option>
+                                        <option value={map_id} key={map_id}>{mapInformation[map_id].map_name}</option>
                                     ))}
                                 </select>
                             </div>
@@ -58,15 +188,18 @@ function PredictFilter() {
 
 
                         <div className='filter-option tile-box'>
-                            <p>Score Tiles</p>
+                            <p>Score Tiles: {scoreTileList.length}/6</p>
                             <div className='score-tile-selections'>
                                 {scoreTiles.map((tile) => (
                                     <label key={tile} >
                                         <input
-                                        type='checkbox'
-                                        value={tile}
-                                        
-                                           
+                                            type='checkbox'
+                                            value={tile}
+                                            onChange={handleScoreTileChange}
+                                            checked={scoreTileList.includes(tile)}
+                                            disabled={!numPlayers}
+
+
                                         />
                                         <img className='filter-score-tile-img' src={tileInfo['scoreTiles'][tile].img}></img>
                                     </label>
@@ -76,15 +209,18 @@ function PredictFilter() {
 
 
                         <div className='filter-option bonus-tile-box'>
-                            <p>Bonus Tiles</p>
+                            <p>Bonus Tiles: {bonusTileList.length}/{bonusTileAmounts[numPlayers]}</p>
                             <div className='bonus-tile-selections'>
                                 {bonusTiles.map((tile) => (
                                     <label key={tile} >
                                         <input
-                                        type='checkbox'
-                                        value={tile}
-                                        
-                                           
+                                            type='checkbox'
+                                            value={tile}
+                                            onChange={handleBonusTileChange}
+                                            checked={bonusTileList.includes(tile)}
+                                            disabled={!numPlayers}
+
+
                                         />
                                         <img className='filter-bonus-tile-img' src={tileInfo['bonusTiles'][tile].img}></img>
                                     </label>
@@ -92,17 +228,20 @@ function PredictFilter() {
                             </div>
                         </div>
 
-
-                           <div className='filter-option tile-box'>
+                        {/*
+                        {factionsIncluded ?
+                        <div className='filter-option tile-box'>
                             <p>Factions</p>
                             <div className='score-tile-selections'>
                                 {factions.map((f) => (
-                                    <label style={{backgroundColor: factionInformation[f].color}} key={f} >
+                                    <label className='faction-label' style={{ backgroundColor: factionInformation[f].color }} key={f} >
                                         <input
-                                        type='checkbox'
-                                        value={f}
-                                        
-                                           
+                                            type='checkbox'
+                                            value={f}
+                                            onChange={handleFactionListChange}
+                                            checked={factionList.includes(f)}
+
+
                                         />
                                         <p>{factionInformation[f].name}</p>
                                         <img className='prediction-faction-img' src={factionImages[factionInformation[f].name.split(' ').join('')]}></img>
@@ -110,7 +249,9 @@ function PredictFilter() {
                                 ))}
                             </div>
                         </div>
+                        : null}
 
+                        */}
 
 
 
